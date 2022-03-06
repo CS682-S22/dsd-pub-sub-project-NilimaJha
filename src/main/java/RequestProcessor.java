b69251fe5c45cb4ef932ad1d990e3dd56aca2b77
Nilima Jha <njha2@dons.usfca.edu>
2022-03-06 15:28:25 -0800

    Add RequestProcessor class.

--- src/main/java/RequestProcessor.java
+++ src/main/java/RequestProcessor.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class RequestProcessor {
+public class RequestProcessor implements Runnable {
+    private Connection connection;
+    private Data data;
+
+    /**
+     * Constructor that initialises Connection class object and also Data
+     * @param connection
+     */
+    public RequestProcessor(Connection connection) {
+        this.connection = connection;
+        this.data = DataInitializer.getData();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+
+        }
+    }
+
+    public void start() {
+
+    }
 }
