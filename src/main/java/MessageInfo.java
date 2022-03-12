b5dacb4588be9f8fb30ea2d019ef39fe3aba2413
Nilima Jha <njha2@dons.usfca.edu>
2022-03-11 21:50:17 -0800

    Create a class to store all the published message and its offset numbers.

---
+++ src/main/java/MessageInfo.java
@@ -0,0 +1,153 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class MessageInfo {
+    private ArrayList<Integer> flushedMessageOffset;
+    private ArrayList<Integer> inMemoryMessageOffset;
+    private ArrayList<byte[]> inMemoryMessage;
+    private int lastOffSet = 0; // updated after adding each message
+    private FileOutputStream fileWriter = null;
+    private FileInputStream fileReader = null;
+    private String topicFileName;
+    final ReentrantReadWriteLock lock1 = new ReentrantReadWriteLock();
+    final ReentrantReadWriteLock lock2 = new ReentrantReadWriteLock();
+
+
+    /**
+     * Constructor to initialise class attributes.
+     * @param topic
+     */
+    public MessageInfo(String topic) {
+        this.flushedMessageOffset = new ArrayList<>();
+        this.inMemoryMessageOffset = new ArrayList<>();
+        this.inMemoryMessage = new ArrayList<>();
+        this.topicFileName = topic + ".txt";
+        fileWriterInitializer(this.topicFileName);
+        fileReaderInitializer(this.topicFileName);
+    }
+
+    /**
+     * initialises the FileOutputStream named fileWriter of the class and deletes the file if already exist.
+     * @param fileName
+     */
+    public void fileWriterInitializer(String fileName) {
+        File segmentFile = new File(fileName);
+        if(segmentFile.exists()){
+            segmentFile.delete(); //deleting file if exist
+        }
+        try {
+            fileWriter = new FileOutputStream(fileName, true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * initialises the FileInputStream named fileReader of the class.
+     * @param fileName
+     */
+    private void fileReaderInitializer(String fileName) {
+        try {
+            this.fileReader = new FileInputStream(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * appending new message published by publisher to the inMemory buffer ArrayList.
+     * and if after adding new message buffer ArrayList is full then
+     * flushing those inMemory message to the file on the disk and
+     * making it available for the consumer.
+     * @param message
+     * @return true
+     */
+    public boolean addNewMessage(byte[] message) {
+        // acquire write lock on inMemoryOffset Arraylist and inMemoryMessage ArrayList
+        this.lock1.writeLock().lock();
+        this.inMemoryMessageOffset.add(this.lastOffSet);
+        this.inMemoryMessage.add(message);
+        this.lastOffSet += message.length;
+        if (this.inMemoryMessageOffset.size() == Constants.TOTAL_IN_MEMORY_MESSAGE_SIZE) {
+            flushOnFile();
+        }
+        // clearing in-memory  buffer of the published message.
+        this.inMemoryMessageOffset.clear();
+        this.inMemoryMessage.clear();
+        // release write lock on inMemoryOffset Arraylist and inMemoryMessage ArrayList.
+        this.lock1.writeLock().unlock();
+        return true;
+    }
+
+    /**
+     * writing inMemory data on the file using FileOutputStream named fileWriter.
+     */
+    public void flushOnFile() {
+        // acquire write lock on the file and flushedMessageOffset ArrayList
+        this.lock2.writeLock().lock();
+        //flushing data on the file on file
+        for (byte[] eachMessageByteArray : this.inMemoryMessage) {
+            try {
+                this.fileWriter.write(eachMessageByteArray);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        this.flushedMessageOffset.addAll(this.inMemoryMessageOffset);
+        // realising write lock on the file and flushedMessageOffset ArrayList
+        this.lock2.writeLock().unlock();
+    }
+
+    /**
+     * reads 10 message from the offset given and returns it.
+     * @param offSet
+     * @return messageBatch
+     */
+    public ArrayList<byte[]> getMessage(int offSet) {
+        this.lock2.readLock().lock();
+        ArrayList<byte[]> messageBatch = null;
+        int count = 0;
+        int currentOffset = offSet;
+        // get the current offset index in the flushedMessageOffset ArrayList
+        int index = this.flushedMessageOffset.indexOf(offSet);
+        // offset is not available
+        if (index == -1) {
+            this.lock2.readLock().unlock();
+            return messageBatch;
+        } else {
+            messageBatch = new ArrayList<>();
+        }
+        while (count < 10 && currentOffset <= this.flushedMessageOffset.get(this.flushedMessageOffset.size() - 1)) {
+            // read one message at a time and append it on the messageBatch arrayList
+            byte[] eachMessage = new byte[0];
+            if (index == this.flushedMessageOffset.size() - 1) {
+                try {
+                    eachMessage = new byte[this.fileReader.available() - currentOffset];
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                eachMessage = new byte[this.flushedMessageOffset.get(index + 1) - currentOffset];
+            }
+
+            try {
+                this.fileReader.getChannel().position(offSet);
+                fileReader.read(eachMessage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            messageBatch.add(eachMessage);
+            count++;
+            if (index == this.flushedMessageOffset.size() - 1) {
+                currentOffset = this.flushedMessageOffset.get(index) + 1;
+                index++;
+            } else {
+                index++;
+                currentOffset = this.flushedMessageOffset.get(index);
+            }
+        }
+        this.lock2.readLock().unlock();
+        return messageBatch;
+    }
+}
