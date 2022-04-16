44c43242c8d7ec77d4737eaa1187999828546bcf
Nilima Jha <njha2@dons.usfca.edu>
2022-04-15 18:56:10 -0700

    Add required proto for communication between brokers and with loadbalancer.

---
+++ src/main/java/HeartBeatModule.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HeartBeatModule {
+    private ConcurrentHashMap<Integer, > HBReceiveTimes = new ConcurrentHashMap();
+}
---
+++ src/main/java/customeException/ConnectionClosedException.java
@@ -0,0 +1,5 @@
+public class ConnectionClosedException extends Exception{
+    public ConnectionClosedException() {
+        
+    }
+}
