b621bdb1598ccb3c38f84cb52013caa1d916b26d
Nilima Jha <njha2@dons.usfca.edu>
2022-04-22 11:40:40 -0700

    Update the coccumentation for the Custom exception class.

--- src/main/java/customeException/ConnectionClosedException.java
+++ src/main/java/customeException/ConnectionClosedException.java
@@ -1,5 +1,9 @@
 package customeException;
 
+/**
+ * Custom Exception handler
+ * @author nilimajha
+ */
 public class ConnectionClosedException extends Exception{
     public ConnectionClosedException(String message) {
         super(message);
